bd31251e8f024ad5d74ffcdbcff5e48ecfe41d79
Njiraini23 <njiraini88@gmail.com>
2023-08-19 18:27:39 +0000

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
---Creates a table user that takes the ID as integer
---email(string 255 chars), name(string 255 chars)
+-- creates a table user that takes the ID as integer
+-- email(string 255 chars), name(string 255 chars)
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
