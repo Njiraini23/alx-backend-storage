9ae4730a19789d95bc17c0d0510356b4ef26f86b
Njiraini23 <njiraini88@gmail.com>
2023-08-20 18:56:05 +0000

    5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,11 @@
--- A script to creates a trigger that resets attribute
--- valid_email only when email has been changed
+-- Script that creates a trigger that resets the attribute valid_email
+-- only when the email has been changed
 DELIMITER //
-CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users 
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 	IF NEW.email != OLD.email THEN
 		SET NEW.valid_email = 0;
 	END IF;
 END;//
-DELIMITER;
+DELIMITER ;
