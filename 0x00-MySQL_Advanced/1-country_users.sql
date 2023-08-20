1a1b792e2a57f2c82c221f690a5ac5f3e0b5848d
Njiraini23 <njiraini88@gmail.com>
2023-08-20 13:35:52 +0000

    fixes

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
---- Write an SQL script with these requirements
---- id INT not null aoto increment and primary Key`
+--- creates a table users following these requirements
+--- id INT not null aoto increment and primary Key
 CREATE TABLE IF NOT EXISTS users (
-	id INTEGER NOT NULL PRIMARY KEY AUTO INCREMENT,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
