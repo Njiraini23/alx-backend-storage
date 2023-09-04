7eef0b470b0ca77109e3674312e606f4681478b2
Njiraini23 <njiraini88@gmail.com>
2023-09-04 06:44:01 +0000

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,3 +22,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn:
+            Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+        """return data as a byte string when retrieved from the server"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is None:
+            return data
+        return fn(data)
+
+    def get_str(self, key:str) -> Union[str, None]:
+        """methods that automatically paratrize Cache.get"""
+        data = self._redis.get(key)
+        return data.decode('utf-8')
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """metho with Cache.get with the right conversion function"""
+        return self.get(key, fn=int)
+
