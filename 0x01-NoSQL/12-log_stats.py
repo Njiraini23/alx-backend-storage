b5a7f955d754b5e4a34b3162d024ad1da18064b8
Njiraini23 <njiraini88@gmail.com>
2024-01-10 04:45:01 -0800

    corrections on log stats file

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,40 +1,23 @@
 #!/usr/bin/env python3
-"""
-A script that provides some stats about Nginx
-logs stored in MongoDB with Database logs stored
-in collection in nginx. First line X logs and second
-line is Methods
-"""
+""" MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-
-def get_logs_stats(collection, option=None):
-    """
-    this function shows stats for logs
-    """
-    if option:
-        count = collection.count_documents({"method": option})
-        print("\tmethod {}: {}".format(option, count))
-        return
-
-    total_logs = collection.count_documents({})
-    print("{} logs".format(total_logs))
-    print("Methods:")
-    for method in METHODS:
-        get_logs_stats(collection, method)
-    status_check_count = collection.count_documents(
-            {"method": "GET", "path": "/status"}
-    )
-    print("{} status check".format(status_check_count))
-
-
 if __name__ == "__main__":
-    """Using the MongoDB connection URL"""
-    mongodb_url = 'mongodb://127.0.0.127017'
-    client = MongoClient(mongodb_url)
-    db = client.logs
-    nginx_collection = db.nginx
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-    get_logs_stats(nginx_collection)
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
